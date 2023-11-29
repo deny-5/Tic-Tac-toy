fa3e1771d564e02c615f9d59cd6a76dac47b8a37
deny-5 <kodenis518@gmail.com>
2023-11-29 16:05:30 +0300

    feat: add logic for process users touches

--- index.js
+++ index.js
@@ -1,8 +1,54 @@
+//          GAME_MAIN_CODE
+
+let cell_1 = document.getElementById('num-1');
+cell_1.addEventListener('click',gameMove);
+cell_1.addEventListener('click',()=>{cell_1.setAttribute('style', `background-image: url('./Group 3.png') ;
+background-size: 90px 90px;`)});
+
+let cell_2 = document.getElementById('num-2');
+cell_2.addEventListener('click',gameMove);
+cell_2.addEventListener('click',()=>{cell_2.setAttribute('style', `background-image: url('./Group 3.png') ;
+background-size: 90px 90px;`)})
+
+let cell_3 = document.getElementById('num-3');
+cell_3.addEventListener('click',gameMove);
+cell_3.addEventListener('click',()=>{cell_3.setAttribute('style', `background-image: url('./Group 3.png') ;
+background-size: 90px 90px;`)})
+
+let cell_4 = document.getElementById('num-4');
+cell_4.addEventListener('click',gameMove);
+cell_4.addEventListener('click',()=>{cell_4.setAttribute('style', `background-image: url('./Group 3.png') ;
+background-size: 90px 90px;`)})
+
+let cell_5 = document.getElementById('num-5');
+cell_5.addEventListener('click',gameMove);
+cell_5.addEventListener('click',()=>{cell_5.setAttribute('style', `background-image: url('./Group 3.png') ;
+background-size: 90px 90px;`)})
+
+let cell_6 = document.getElementById('num-6');
+cell_6.addEventListener('click',gameMove);
+cell_6.addEventListener('click',()=>{cell_6.setAttribute('style', `background-image: url('./Group 3.png') ;
+background-size: 90px 90px;`)})
+
+let cell_7 = document.getElementById('num-7');
+cell_7.addEventListener('click',gameMove);
+cell_7.addEventListener('click',()=>{cell_7.setAttribute('style', `background-image: url('./Group 3.png') ;
+background-size: 90px 90px;`)})
+
+let cell_8 = document.getElementById('num-8');
+cell_8.addEventListener('click',gameMove);
+cell_8.addEventListener('click',()=>{cell_8.setAttribute('style', `background-image: url('./Group 3.png') ;
+background-size: 90px 90px;`)})
+
+let cell_9 = document.getElementById('num-9');
+cell_9.addEventListener('click',gameMove);
+cell_9.addEventListener('click',()=>{cell_9.setAttribute('style', `background-image: url('./Group 3.png') ;
+background-size: 90px 90px;`)})
+
 let button = document.getElementById('button');
 button.addEventListener('click',gameMove);
-
 let clickCounter = button.getAttribute('value');
-gridInit();
+// gridInit();
 
 // Function for make next move in game.
 
@@ -25,22 +71,29 @@ function gameMove(){
 
 };
 
-function gridInit() {
+// function gridInit() {
 
-    let color = 'rgb(187, 199, 209)';
+//     let color = 'rgb(187, 199, 209)';
 
-    let elem = document.getElementById("grid");
-    elem.setAttribute(`style`,`display: grid;
-                               grid-template-rows:
-                               repeat(3, 100px);
-                               grid-template-columns: 
-                               repeat(3, 100px);
-                               background-color: ${color};`);
+//     let elem = document.getElementById("grid");
+//     elem.setAttribute(`style`,`display: grid;
+//                                grid-template-rows:
+//                                repeat(3, 100px);
+//                                grid-template-columns: 
+//                                repeat(3, 100px);
+//                                background-color: ${color};`);
                                
-    for (let i = 1 ; i <= 9 ; i++) {
+//     for (let i = 1 ; i <= 9 ; i++) {
         
-        let newdiv = document.createElement("div");
-        newdiv.className = `${i}_grid_el`;
-        elem.append(newdiv);       
-    }
+//         let newdiv = document.createElement("div");
+//         newdiv.className = `${i}_grid_el`;
+//         elem.append(newdiv);       
+//     }
+// }
+
+function gridTouch() {
+
+    let cell = self;
+    cell.setAttribute('style', `background-image: url('./Group 3.png') ;`)
+
 }
