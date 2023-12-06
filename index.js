56efe39744f7e8ae0a1baf8f723b555bc21d6ffb
deny-5 <kodenis518@gmail.com>
2023-12-06 22:30:46 +0300

    Feat, Fix, Refactor: change whole logic and design

--- index.js
+++ index.js
@@ -1,72 +1,105 @@
-//          GAME_MAIN_CODE
+let canMove = true;
+let words = ['YOUR TURN', 'MY TURN', 'ME WIN', 'YOU WIN'];
+let emptyCells = [];
 
-let cell_1 = document.getElementById('num-1');
-cell_1.addEventListener('click',gameMove);
-cell_1.addEventListener('click',()=>{cell_1.setAttribute('style', `background-image: url('./Group 3.png') ;
-background-size: 90px 90px;`)});
+let mainElem = document.getElementById('wrapper');
 
-let cell_2 = document.getElementById('num-2');
-cell_2.addEventListener('click',gameMove);
-cell_2.addEventListener('click',()=>{cell_2.setAttribute('style', `background-image: url('./Group 3.png') ;
-background-size: 90px 90px;`)})
+let container = document.createElement('div');
+container.className = 'grid';
+mainElem.appendChild(container);
 
-let cell_3 = document.getElementById('num-3');
-cell_3.addEventListener('click',gameMove);
-cell_3.addEventListener('click',()=>{cell_3.setAttribute('style', `background-image: url('./Group 3.png') ;
-background-size: 90px 90px;`)})
-
-let cell_4 = document.getElementById('num-4');
-cell_4.addEventListener('click',gameMove);
-cell_4.addEventListener('click',()=>{cell_4.setAttribute('style', `background-image: url('./Group 3.png') ;
-background-size: 90px 90px;`)})
-
-let cell_5 = document.getElementById('num-5');
-cell_5.addEventListener('click',gameMove);
-cell_5.addEventListener('click',()=>{cell_5.setAttribute('style', `background-image: url('./Group 3.png') ;
-background-size: 90px 90px;`)})
-
-let cell_6 = document.getElementById('num-6');
-cell_6.addEventListener('click',gameMove);
-cell_6.addEventListener('click',()=>{cell_6.setAttribute('style', `background-image: url('./Group 3.png') ;
-background-size: 90px 90px;`)})
-
-let cell_7 = document.getElementById('num-7');
-cell_7.addEventListener('click',gameMove);
-cell_7.addEventListener('click',()=>{cell_7.setAttribute('style', `background-image: url('./Group 3.png') ;
-background-size: 90px 90px;`)})
-
-let cell_8 = document.getElementById('num-8');
-cell_8.addEventListener('click',gameMove);
-cell_8.addEventListener('click',()=>{cell_8.setAttribute('style', `background-image: url('./Group 3.png') ;
-background-size: 90px 90px;`)})
-
-let cell_9 = document.getElementById('num-9');
-cell_9.addEventListener('click',gameMove);
-cell_9.addEventListener('click',()=>{cell_9.setAttribute('style', `background-image: url('./Group 3.png') ;
-background-size: 90px 90px;`)})
-
-let button = document.getElementById('button');
-button.addEventListener('click',gameMove);
-let clickCounter = button.getAttribute('value');
+let gameMove = document.createElement('button');
+gameMove.innerHTML = words[0];
+gameMove.id = 'mainButton';
+mainElem.appendChild(gameMove);
+gameMove.addEventListener('click', ()=>{botRandom()})
 
 
-// Function for make next move in game.
 
-function gameMove(){
+let field = 
+{
+	cell1 : document.createElement('button'),
+	cell2 : document.createElement('button'),
+	cell3 : document.createElement('button'),
+	
+	cell4 : document.createElement('button'),
+	cell5 : document.createElement('button'),
+	cell6 : document.createElement('button'),
+	
+	cell7 : document.createElement('button'),
+	cell8 : document.createElement('button'),
+	cell9 : document.createElement('button'),
+}
 
-    let string1 = "YOUR TURN";
-    let string2 = "MY TURN";
-    let stringVictory = "YOU WIN";
-    let stringLost = "I WIN";
+function addField(container)
+{
+	for (let key in this)
+	{
+		container.append(this[key]);
+	}
+}
 
-    if (clickCounter != "YOUR TURN") {
-        //BINDING WITH OTHER GAME LOGIC
-        clickCounter = string1;
-        button.setAttribute('value', `${string2}`);
-    } else if(clickCounter != "MY TURN"){
-        //BINDING WITH OTHER GAME LOGIC
-        clickCounter = string2;
-        button.setAttribute('value', `${string1}`);
+function buttonNumerate(obj)
+{
+	let i = 1;
+	for (let key in obj)
+	{
+		// obj[key].innerHTML = i;
+		obj[key].setAttribute('id', `${i}-cell`);
+		obj[key].setAttribute('class', `inactive`);
+		obj[key].addEventListener('click', ()=>{buttonAction(obj[key])});
+		i++;
+	}
+}
+
+function buttonAction(btn)
+{
+	let checked = btn.getAttribute('class');
+	if(checked == `inactive` && canMove && btn.innerHTML != 'O')
+	{
+		btn.setAttribute('class',`active`);
+        btn.innerHTML = "X";
+        gameMove.innerHTML = words[1];
+        canMove = false;
+    }else if (checked == `active` && !canMove)
+	{
+		btn.setAttribute('class',`inactive`);
+        btn.innerHTML = "";
+        gameMove.innerHTML = words[0];
+        canMove = true;
+	}
+}
+
+function findEmpty(fieldObj, arr)
+{
+    for (const key in fieldObj)
+    {
+        if(fieldObj[key].innerHTML != 'X' && fieldObj[key].innerHTML != 'O')
+        {
+            arr.push(key);
+        }
     }
+    console.log(arr);
+    return arr;
+}
 
-};
+function chooseEmpty(num)
+{
+    return Math.floor(Math.random() * num);
+}
+
+function botRandom()
+{
+    if(!canMove)
+    {
+        let numOfEmpty = findEmpty(field,emptyCells);
+        let chosenOneNumber = chooseEmpty(numOfEmpty.length);
+        field[numOfEmpty[chosenOneNumber]].innerHTML = 'O';
+        canMove = true;
+        gameMove.innerHTML = words[0];
+        emptyCells = [];
+    }
+}
+
+buttonNumerate(field);
+addField.call(field, container);
